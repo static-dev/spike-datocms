aba34400cfbe28b604713a0e60e07c0061722539
Jeff Escalante <escalantejeff@gmail.com>
2017-04-13 18:27:46 -0400

    single templates

--- test/index.js
+++ test/index.js
@@ -4,6 +4,7 @@ const Spike = require('spike-core')
 const htmlStandards = require('reshape-standard')
 const path = require('path')
 const fs = require('fs')
+const rimraf = require('rimraf')
 
 const fixturesPath = path.join(__dirname, 'fixtures')
 const datoToken = 'cb1f960dfb4f14a7ae93'
@@ -40,6 +41,42 @@ test.cb('works with spike', (t) => {
   project.on('compile', () => {
     const output = JSON.parse(fs.readFileSync(path.join(projPath, 'public/index.html'), 'utf8'))
     t.truthy(output.article.length > 0)
+    rimraf.sync(path.join(projPath, 'public'))
+    t.end()
+  })
+
+  project.compile()
+})
+
+test.todo('generates josn correctly')
+
+test.cb('generates single page templates correctly', (t) => {
+  const locals = {}
+  const projPath = path.join(fixturesPath, 'template')
+  const project = new Spike({
+    root: projPath,
+    reshape: htmlStandards({ parser: false, locals: () => locals }),
+    ignore: ['template.html'],
+    plugins: [new SpikeDatoCMS({
+      token: datoToken,
+      addDataTo: locals,
+      models: [{
+        name: 'article',
+        template: {
+          path: 'template.html',
+          output: (obj) => `articles/${obj.slug}.html`
+        }
+      }]
+    })]
+  })
+
+  project.on('error', t.end)
+  project.on('compile', () => {
+    const file1 = JSON.parse(fs.readFileSync(path.join(projPath, 'public/articles/testing-post.html'), 'utf8'))
+    const file2 = JSON.parse(fs.readFileSync(path.join(projPath, 'public/articles/testing-2-post.html'), 'utf8'))
+    t.is(file1.title, 'Testing Post')
+    t.is(file2.title, 'Testing #2 Post')
+    rimraf.sync(path.join(projPath, 'public'))
     t.end()
   })
 
